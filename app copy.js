c638cc182ec7f8dcdcb0eb97901e4f798d34367a
Alex Barbosa <alexamericob@hotmail.com>
2026-02-10 10:29:08 -0300

    exp Treinamento 02

--- app copy.js
+++ app copy.js
@@ -1,4 +1,4 @@
-let numeroSecreto = parseInt(Math.random() *100 + 2);
+let numeroSecreto = parseInt(Math.random() *100 + 1);
 // Armazena o número escolhido pelo usuário
 let chute;
 let tentativas = 1;
